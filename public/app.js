53819cf5e1bf3350978d397322740c7042af5c40
niloyansella <nilo.sellathurai@student.rocva.nl>
2022-07-12 12:17:54 +0200

    Version 1.0.4

--- public/app.js
+++ public/app.js
@@ -2,8 +2,12 @@
 
 const changeBtn = document.getElementById("detailschange");
 const changeForm = document.getElementById("change-detailsarea");
+const menuModal = document.getElementById("header--menu-moadal");
+// changeBtn.addEventListener("click", () => {
+//   console.log("Change Now.....");
+//   changeForm.style.visibility = "visible";
+// });
 
-changeBtn.addEventListener("click", () => {
-  console.log("Change Now.....");
-  changeForm.style.visibility = "visible";
-});
+const openModalMenu = () => {
+  menuModal.style.visibility = "visible";
+};
